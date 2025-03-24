f132cd8c6d75b2cec4ac319878ac7d79bf76471a
KazeFreeze <tapirua@gmail.com>
2025-03-24 20:09:17 +0800

    Refactor project structure: remove main.py, add subtitle_converter.py and setup.py, update .gitignore

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="SubtitleConverter",
+    version="1.0.0",
+    author="Your Name",
+    description="JSON to Subtitle Converter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
